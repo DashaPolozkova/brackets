86bba07123c7bad6c0ed4666c6264bf5cd297a2a
Dasha Polozkova <dasha.polozkova@gmail.com>
2022-02-19 02:18:15 +0300

    new solution

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  let stack = []
-  let openBrackets = []
+   let stack = []
+  let closeBrackets = []
   let bracketsPair = {}
   for (let j = 0; j < bracketsConfig.length; j++) {
-    openBrackets.push(bracketsConfig[j][0])
+     closeBrackets.push(bracketsConfig[j][1])
     bracketsPair[bracketsConfig[j][1]] = bracketsConfig[j][0]
   }
  for (let i = 0; i < str.length; i++) {
    let currSymbol = str[i]
-   if (openBrackets.includes(currSymbol)) {
-     stack.push(currSymbol)
-   } else {
-     if (stack.length === 0) return false
-     }
-     }
+   let topElement = stack[stack.length - 1]
 
-     let topElement = stack[stack.length - 1]
+   if (closeBrackets.includes(currSymbol)) {
      if (bracketsPair[currSymbol] === topElement) {
-       stack.pop()
+      stack.pop()
      } else {
-       return false
+       stack.push(currSymbol)
      }
+    
+   } else {
+    stack.push(currSymbol)
+   }
+ }
      return stack.length === 0
 }
-
-console.log(check('()', config1))
-
-const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+ 
